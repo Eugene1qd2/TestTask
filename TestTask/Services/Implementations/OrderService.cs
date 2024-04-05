03529032060755a17262b9a91c8ab607ed037ae7
Eugene1qd2 <qawsed200412345@gmail.com>
2024-04-05 18:51:27 +0300

    first commit

---
+++ TestTask/Services/Implementations/OrderService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class OrderService : IOrderService
+    {
+        ApplicationDbContext _context;
+        public OrderService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Order> GetOrder()
+        {
+            var result = await _context.Orders
+                .OrderByDescending(od=>od.Price*od.Quantity)
+                .FirstOrDefaultAsync();
+
+            return result;
+        }
+
+        public async Task<List<Order>> GetOrders()
+        {
+            var result = await _context.Orders
+                .Where(od=>od.Quantity>10)
+                .ToListAsync();
+
+            return result;
+        }
+    }
+}
---
+++ TestTask/Services/Implementations/UserService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class UserService : IUserService
+    {
+        ApplicationDbContext _context;
+        public UserService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<User> GetUser()
+        {
+            var result = await (from od in _context.Orders
+                                group od by od.User into g
+                                select g)
+                                .OrderByDescending(gr => gr.Count())
+                                .FirstOrDefaultAsync();
+
+            return result.Key;
+        }
+
+        public async Task<List<User>> GetUsers()
+        {
+            var result=await _context.Users
+                .Where(us=>us.Status==Enums.UserStatus.Inactive)
+                .ToListAsync();
+            return result;
+        }
+    }
+}
